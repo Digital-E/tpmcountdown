4377e65e236d650d710893b91b1155f24bd55cd4
SamuelBassett <samabassett@gmail.com>
2022-06-07 14:31:54 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    var timerId =
-    countdown(
-    new Date(2022, 5, 23),
-    function(ts) {
-     document.getElementById('pageTimer').innerHTML = ts.toHTML("strong");
-    },
-    countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
-
     countdown.setLabels(
         ' milliseconde| seconde| minute| heure| jour| semaine| mois| année| décennie| siècle| millénaire',
         ' millisecondes| secondes| minutes| heures| jours| semaines| mois| années| décennies| siècles| millénaires',
@@ -15,4 +7,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
         ', ',
         'maintenant');
 
+    countdown(
+    new Date(2022, 5, 23),
+    function(ts) {
+     document.getElementById('pageTimer').innerHTML = ts.toHTML("strong");
+    },
+    countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
+        
 });
